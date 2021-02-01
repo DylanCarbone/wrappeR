ce15c035db5f0c384c74e2332ab57399da231a0f
Logie <marlog@ceh.ac.uk>
2021-02-01 14:46:43 +0000

    Creating function for createmetadata script

--- R/createMetadata.r
+++ R/createMetadata.r
@@ -1,20 +1,19 @@
-library(dplyr)
-library(pbapply)
-objst <- '/data-s3/occmods'
-list.files(objst)
-metadata <- lapply(list.files(objst)[1:3], function(taxa){
-  cat('Creating metadata for',taxa,'\n')
-  data_types <- list.files(file.path(objst, taxa))
-  ds <- pblapply(data_types, FUN = function(data_type){
-    datasets <- list.files(file.path(objst, taxa, data_type))
-    years <- str_extract(string = datasets, pattern = '[0-9]+') %>% as.numeric()
-    data.frame(taxa = taxa,
-               data_type = data_type,
-               data_location = file.path(objst, taxa, data_type, datasets),
-               dataset_name = datasets,
-               most_recent = (years==max(years)),
-               stringsAsFactors = FALSE)
+createMetadata <- function(file_location){
+  library(dplyr)
+  library(pbapply)
+  metadata <- lapply(list.files(file_location), function(taxa){
+    cat('Creating metadata for',taxa,'\n')
+    data_types <- list.files(file.path(file_location, taxa))
+    ds <- pblapply(data_types, FUN = function(data_type){
+      datasets <- list.files(file.path(file_location, taxa, data_type))
+      years <- str_extract(string = datasets, pattern = '[0-9]+') %>% as.numeric()
+      data.frame(taxa = taxa,
+                 data_type = data_type,
+                 data_location = file.path(file_location, taxa, data_type, datasets),
+                 dataset_name = datasets,
+                 most_recent = (years==max(years)),
+                 stringsAsFactors = FALSE)
+    }) %>% bind_rows()
   }) %>% bind_rows()
-}) %>% bind_rows()
-
-saveRDS(metadata, 'metadata.rds')
+  return(metadata)
+}
