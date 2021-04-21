070282cfe021379a161319759c88ef8ec80cb977
Cooke <roboke@ceh.ac.uk>
2021-04-21 16:02:10 +0100

    updated documentation

--- R/createRoster.R
+++ R/createRoster.R
@@ -1,14 +1,14 @@
 #' \code{createRoster} - Specify how to filter the occupancy model outputs.
 #' 
 #' @description This function can be used to specify the filters that will be applied to 
-#'              the occupancy model outputs using \code{applyFilters}. It works by creating a list
-#'              of 1-row dataframes with all the information needed for \code{applyFilters}. This list 
-#'              is then applied to the applyFilters function later. Arguments should be provided
+#'              the occupancy model outputs using \code{applySamp}. It works by creating a list
+#'              of 1-row dataframes with all the information needed for \code{applySamp}. This list 
+#'              is then applied to the applySamp function later. Arguments should be provided
 #'              as vectors of equal length, with each element in the vectors corresponding to one 
-#'              call to \code{applyFilters}. 
+#'              call to \code{applySamp}. 
 #'              
 #' @param index Numeric. Index of the number of taxonomic groups to 
-#'              \code{applyFilters} across.
+#'              \code{applySamp} across.
 #'
 #' @param modPath A character string or vector of strings. Location(s) of the 
 #'                occupancy model outputs. 
@@ -65,7 +65,7 @@
 #'               last years of data for each species or for the whole group, 
 #'               respectively.
 #' 	  
-#' @return A list of 1-row dataframes containing all arguments needed for \code{applyFilters}. \code{applyFilters}
+#' @return A list of 1-row dataframes containing all arguments needed for \code{applySamp}. \code{applySamp}
 #'         can then be applied to this list to filter models outputs for different taxonomic groups, which may 
 #'         come from different rounds (e.g. Charlie's or later), for different regions, etc. 
 #'         
